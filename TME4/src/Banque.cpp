4f41cba6b967e126e164d1676621aae48ec927ad
Damien <damien.albisson@etu.sorbonne-universite.fr>
2022-10-15 12:04:48 +0200

    Fin TME4, poser la question au prof pour les const sur le mutex de compte qui est pourtant mutable, il fait chier

--- TME4/src/Banque.cpp
+++ TME4/src/Banque.cpp
@@ -8,7 +8,7 @@ namespace pr {
 
 	void Banque::transfert(size_t deb, size_t cred, unsigned int val) {
 		
-		
+		//m.lock();
 		if (deb < cred) {
 			comptes[deb].getMutex().lock();
 			comptes[cred].getMutex().lock();
@@ -25,21 +25,32 @@ namespace pr {
 
 		comptes[deb].getMutex().unlock();
 		comptes[cred].getMutex().unlock();
+		//m.unlock();
 		
 	}
 	size_t Banque::size() const {
 		return comptes.size();
 	}
-	bool Banque::comptabiliser (int attendu) const {
+	int Banque::getSolde(size_t i) const {
+		return comptes[i].getSolde();
+	}
+	bool Banque::comptabiliser (int attendu) /*const*/ {
+
 		int bilan = 0;
 		int id = 0;
-		for (const auto & compte : comptes) {
+		//m.lock();
+		for (auto & compte : comptes) {
+			compte.getMutex().lock();
 			if (compte.getSolde() < 0) {
 				cout << "Compte " << id << " en négatif : " << compte.getSolde() << endl;
 			}
 			bilan += compte.getSolde();
 			id++;
 		}
+		for (auto & compte : comptes) {
+			compte.getMutex().unlock();
+		}
+		//m.unlock();
 		if (bilan != attendu) {
 			cout << "Bilan comptable faux : attendu " << attendu << " obtenu : " << bilan << endl;
 		}
--- TME4/src/main.cpp
+++ TME4/src/main.cpp
@@ -4,6 +4,7 @@
 #include <functional>
 #include <chrono>
 #include <thread>
+#include <iostream>
 #include "Banque.h"
 
 // Question 2 : Crediter et debiter peuvent se faire en même temps
@@ -18,9 +19,20 @@
 // Question 6 : Probleme de deadlock, il pourrait avoir plusieur ordres de lock
 // Dans transfert on met un ordre sur les locks
 
+// Question 7 : un debitage et un creditage peuvent arriver apres ajout du solde de celui ci qui est donc
+// maintenant devenu erroner
+
+// Question 9 : strategie avec le mutex fonctionne mais elimine la concurrence 
+
+// Question 10 : toujours en utilisant mon ordre, j'ai fait attention à ce qu'il n'y ai pas de deadlock
+
 using namespace std;
 
-void work(pr::Banque& bank) {
+const int NB_THREAD = 5;
+const int NB_COMPTES = 10;
+const int BASE_SOLDE = 10000;
+
+void transfert(pr::Banque& bank) {
 	for (int cpt = 0; cpt < 100; cpt++) {
 		size_t nbCompte = bank.size();
 		size_t i = rand() % nbCompte;
@@ -30,32 +42,43 @@ void work(pr::Banque& bank) {
 
 		bank.transfert(i, j, m);
 
-		this_thread::sleep_for(chrono::milliseconds(r));
+		//this_thread::sleep_for(chrono::milliseconds(r));
 	}
 }
 
-const int NB_COMPTES = 10;
-const int NB_THREAD = 5;
+void comptable(pr::Banque& bank) {
+	for (int i = 0; i < 1000; i++) {
+		if (!bank.comptabiliser(NB_COMPTES * BASE_SOLDE)) cout << "probleme" << endl;
+	}
+}
+
 int main () {
 
 	// TODO : creer des threads qui font ce qui est demandé
 
 	vector<thread> threads;
-	pr::Banque bank(NB_COMPTES, 10000);
+	pr::Banque bank(NB_COMPTES, BASE_SOLDE);
 
 	for (size_t i = 0; i < NB_THREAD; i++) {
-		threads.emplace_back(work, std::ref(bank));
+		threads.emplace_back(transfert, std::ref(bank));
 	}
+
+	threads.emplace_back(comptable, std::ref(bank));
 
 	for (auto & t : threads) {
 		t.join();
 	}
 
 	// TODO : tester solde = NB_THREAD * JP
-	/*
+	int soldeExpected = BASE_SOLDE * NB_COMPTES;
+	int solde = 0;
 	for (size_t i = 0; i < bank.size(); i++) {
-		cout
+		cout << bank.getSolde(i) << endl;
+		solde += bank.getSolde(i);
 	}
-	*/
+
+	cout << "solde expected = " << soldeExpected << endl;
+	cout << "solde = " << solde << endl;
+
 	return 0;
 }
